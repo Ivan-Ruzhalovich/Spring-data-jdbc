e9d35112b31eaa7829ab23483deb7962b391cbac
Ivan Ruzhalovich <ruzhalovich@bk.ru>
2025-02-18 14:23:39 +0300

    Tests

--- src/main/java/com/ivan/spring_data_jdbc/entity/Book.java
+++ src/main/java/com/ivan/spring_data_jdbc/entity/Book.java
@@ -7,6 +7,8 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -19,4 +21,16 @@ public class Book {
     private String author;
     private Integer publicationYear;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id) && Objects.equals(title, book.title) && Objects.equals(author, book.author) && Objects.equals(publicationYear, book.publicationYear);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, author, publicationYear);
+    }
 }
